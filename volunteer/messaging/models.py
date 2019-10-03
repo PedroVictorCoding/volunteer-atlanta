0c1b7de516886ffda24c2e1bbc121dfa1979c702
Pedro <pedro.luis.almeida.victor@protonmail.com>
2019-10-02 20:29:17 -0400

    ws to wss attempt #1

--- volunteer/messaging/models.py
+++ volunteer/messaging/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete='')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_50_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:50]
