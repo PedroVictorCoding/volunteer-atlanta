3249f3250a2cba9deea2028b5aa18d409fb00f65
Pedro <pedro.luis.almeida.victor@protonmail.com>
2019-10-22 23:50:02 -0400

    club files

---
+++ volunteer/clubs/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
+from .views import home
+
+urlpatterns = [
+    path('', home),
+]
