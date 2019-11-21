21366a9e78446da14e1ff82d197d2b13592e5b6d
Pedro Victor <pedro.luis.almeida.victor@protonmail.com>
2019-11-21 17:55:23 -0500

    Cheerpage init

--- volunteer/clubs/views.py
+++ volunteer/clubs/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+import json
+from accounts.models import UserProfile
 
 def home(request):
     return render(request, 'clubs/home.html')
 
 def robotics(request):
     return render(request, 'clubs/robotics/home.html')
+
+
+def cheer_home(request, *args, **kwargs):
+    if request.is_ajax() and request.method == "POST":
+      club = request.POST['ClubEntered']
+      user_profile = UserProfile.objects.get(user=request.user)
+      if user_profile.clubs == "":
+          user_profile.clubs = club
+      elif club in user_profile.clubs:
+          pass
+      else:
+          user_profile.clubs = user_profile.clubs + "," + club
+      user_profile.save()
+      return_val = "true"
+      print("Joined club: " + club)
+      return HttpResponse(json.dumps({'return_val': return_val, 'clubs_in': user_profile.clubs}), content_type="application/json")
+    else:
+      return render(request, 'clubs/cheer/home.html')
+
+
+class CheerView(TemplateView):
+    template_name = 'clubs/cheer/.html'
+
+    def get(self, request):
+        if (request.GET.get('joinClub')):
+            print("Joined Club")
+        #form            = CheerForm()
+        #args            = {'form': form, 'posts': posts, 'random_posts': random_posts, 'users': users}
+        return render(request, self.template_name)
